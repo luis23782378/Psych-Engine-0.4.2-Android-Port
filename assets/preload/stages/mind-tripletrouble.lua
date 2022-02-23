106b5b171c0ddc1283b6cc44226c25aac65d1189
luis23782378 <100315821+luis23782378@users.noreply.github.com>
2022-02-23 15:28:33 -0500

    Add files via upload

---
+++ assets/preload/stages/mind-tripletrouble.lua
@@ -0,0 +1,41 @@
+function onCreate()
+        
+        makeLuaSprite('backgroundend', 'mind/background', -450, -400)
+    addLuaSprite('backgroundend', false)
+
+    makeLuaSprite('rocks', 'mind/rocks', -450, -400)
+    addLuaSprite('rocks', false)
+
+    makeLuaSprite('ground', 'mind/platform', -450, -400)
+    addLuaSprite('ground', false)
+
+    makeLuaSprite('eggmanshitlol', 'mind/TormentorBG', -600, -400)
+    addLuaSprite('eggmanshitlol', false)
+
+    makeLuaSprite('backgroundfire2', 'mind/NeurosesBG', -600, -400)
+    addLuaSprite('backgroundfire2', false)
+
+    makeLuaSprite('thatonepartinneuroses', 'mind/TormentorBG', -600, -400)
+    addLuaSprite('thatonepartinneuroses', false)
+
+
+    makeLuaSprite('backgroundfire', 'mind/NeurosesBG', -600, -400)
+    addLuaSprite('backgroundfire', false)
+
+   
+  makeLuaSprite('backgroundlol', 'mind/TormentorBG', -600, -400)
+    addLuaSprite('backgroundlol', false)
+
+  makeLuaSprite('overlay', 'mind/Overlay3', -500, -300)
+    addLuaSprite('overlay', true)
+    scaleObject('overlay', 1.3, 1.3)
+
+  makeAnimatedLuaSprite('OverlayingSpikes', 'mind/OverlayingSpikes', -50, 0)
+  luaSpriteAddAnimationByPrefix('OverlayingSpikes', 'OverlayingSpikes', 'Spikes', 24, true);
+  addLuaSprite('OverlayingSpikes', true)
+  scaleObject('OverlayingSpikes', 0.8, 0.8)
+  setObjectCamera('OverlayingSpikes', 'hud')
+
+
+    close(true);
+end
